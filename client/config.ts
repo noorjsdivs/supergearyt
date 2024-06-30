4513a06d1776ad02ad15c93613fbed8bd796891b
Noor_Mohammad <noor.jsdivs@gmail.com>
2024-06-30 08:44:07 +0600

    production route added

--- client/config.ts
+++ client/config.ts
@@ -6,7 +6,7 @@ const checkConfig = (server: string): Config | {} => {
   switch (server) {
     case "production":
       config = {
-        baseUrl: "",
+        baseUrl: "https://supergearyt.vercel.app/",
       };
       break;
     case "local":
@@ -20,5 +20,5 @@ const checkConfig = (server: string): Config | {} => {
   return config;
 };
 
-export const selectServer = "local";
+export const selectServer = "production";
 export const config = checkConfig(selectServer) as Config;
